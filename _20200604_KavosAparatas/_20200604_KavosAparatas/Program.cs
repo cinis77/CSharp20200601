bdb807c26b390b4316a2dc0bfc7104ba74611cf3
cinis <cinis.dwarnius.minis@gmail.com>
2020-06-04 19:16:31 +0300

    Switch

---
+++ _20200604_KavosAparatas/_20200604_KavosAparatas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200604_KavosAparatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kavos aparatas");
+            Console.WriteLine("1-limonadas");
+            Console.WriteLine("2-arbata");
+            Console.WriteLine("3-kakava");
+            Console.WriteLine("4-kava");
+            Console.WriteLine("5-nieko");
+            Console.WriteLine("Įveskite pasirinkimą");
+            char pasirinkimas = Convert.ToChar(Console.ReadKey().KeyChar);
+            switch (pasirinkimas)
+            {
+                case '1':
+                    Console.WriteLine(" - jus pasirinkote limonada");
+                    break;
+                case '2':
+                    Console.WriteLine(" - jus pasirinkote arbata");
+                    break;
+                case '3':
+                    Console.WriteLine(" - jus pasirinkote kakava");
+                    break;
+                case '4':
+                    Console.WriteLine(" - jus pasirinkote kava");
+                    break;
+                case '5':
+                    Console.WriteLine(" - jus pasirinkote nieko");
+                    break;
+
+                default:
+                    Console.WriteLine(" - neatpazintas elementas");
+                    break;
+            }
+        }
+    }
+}
---
+++ _20200604_switchPirma/_20200604_switchPirma/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200604_switchPirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu");
+            int sk3 = Convert.ToInt32(Console.ReadLine());
+            int sk4 = Convert.ToInt32(Convert.ToChar(Console.ReadKey().KeyChar));
+           
+            switch (sk1)
+            {
+                case 1:
+                    Console.WriteLine(sk1 + sk2 + sk3);
+                    break;
+                case 2:
+                    Console.WriteLine(sk1 - sk3);
+                    break;
+                case 3:
+                    Console.WriteLine(sk2 * sk3);
+                    break;
+            }
+            
+        }
+    }
+}
