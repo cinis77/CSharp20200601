4db8c8f8e89122e1b2c9a91c1e5213a2668e6e19
cinis77 <cinis.dwarnius.minis@gmail.com>
2020-07-20 18:09:27 +0300

    wpf

---
+++ _20200716_WPFPavyzdys/_20200716_WPFPavyzdys/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20200716_WPFPavyzdys
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ManoTekstas.Text = "Labas pasauli";
+        }
+
+        private static void PopupBox_OnOpened(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void PopupBox_OnClosed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PopupBox_OnClosed(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PopupBox_OnOpened(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
